e1277875a8cb942f81046260a971a0dc82b5b90b
Rémy Viniacourt <remy.viniacourt@outlook.fr>
2025-03-26 08:04:54 +0100

    Creation de la liste de mot. Mise en place du jeu permettant à l'utilisateur de taper un mot et de le comparer à la liste. Mise en place d'un système de score.

--- projects/openclassrooms-azertype/script.js
+++ projects/openclassrooms-azertype/script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+console.log("Hello World")
+
+let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0
+
+for (let i = 0; i < listeMots.length; i++) {
+    let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+
+    if (motUtilisateur == listeMots[i]) {
+        console.log("Bravo !")
+        score++
+    } else {
+        console.log("Vous avez fait une erreur de frappe.")
+    }
+}
+console.log(score)
