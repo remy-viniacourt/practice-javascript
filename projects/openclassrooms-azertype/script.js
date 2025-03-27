c52b5bb3e32599297f514f98a849def62f823b50
Rémy Viniacourt <remy.viniacourt@outlook.fr>
2025-03-27 08:02:50 +0100

    Choix possible entre mots et phrases pour le jeu

--- projects/openclassrooms-azertype/script.js
+++ projects/openclassrooms-azertype/script.js
@@ -1,16 +1,38 @@
 console.log("Hello World")
 
 let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"]
+
 let score = 0
 
-for (let i = 0; i < listeMots.length; i++) {
-    let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+let choixUtilisateur = prompt('Choisissez le mode de jeu en tapant "mots" ou "phrases" : ')
 
-    if (motUtilisateur == listeMots[i]) {
-        console.log("Bravo !")
-        score++
-    } else {
-        console.log("Vous avez fait une erreur de frappe.")
+while(choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
+    choixUtilisateur = prompt('Le choix n\'est pas valide. Choisissez le mode de jeu en tapant "mots" ou "phrases" : ')
+}
+
+if (choixUtilisateur == "mots"){
+    for (let i = 0; i < listeMots.length; i++) {
+        let motUtilisateur = prompt('Entrez le mot : ' + listeMots[i])
+
+        if (motUtilisateur === listeMots[i]) {
+            console.log("Bravo !")
+            score++
+        } else {
+            console.log('Vous avez fait une erreur de frappe.')
+        }
+    }
+} else {
+    for (let i = 0; i < listePhrases.length; i++) {
+        let phraseUtilisateur = prompt('Entrez la phrase : ' + listePhrases[i])
+
+        if (phraseUtilisateur === listePhrases[i]) {
+            console.log("Bravo !")
+            score++
+        } else {
+            console.log("Vous avez fait une erreur de frappe.")
+        }
     }
 }
-console.log(score)
+console.log("Votre score final est de : " +score)
+
