15bd92aa2e5e300de9e9dd7830266adc99f58401
Rémy Viniacourt <remy.viniacourt@outlook.fr>
2025-03-28 16:21:30 +0100

    Ajout des fonctions

--- projects/openclassrooms-azertype/script.js
+++ projects/openclassrooms-azertype/script.js
@@ -1,38 +1,38 @@
-console.log("Hello World")
-
-let listeMots = ["Cachalot", "Pétunia", "Serviette"]
-let listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"]
-
-let score = 0
-
-let choixUtilisateur = prompt('Choisissez le mode de jeu en tapant "mots" ou "phrases" : ')
-
-while(choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
-    choixUtilisateur = prompt('Le choix n\'est pas valide. Choisissez le mode de jeu en tapant "mots" ou "phrases" : ')
+function afficherResultat(resultat, nombreTotalMots) {
+    console.log('Votre score est de ' + resultat + ' sur ' + nombreTotalMots)
 }
 
-if (choixUtilisateur == "mots"){
-    for (let i = 0; i < listeMots.length; i++) {
-        let motUtilisateur = prompt('Entrez le mot : ' + listeMots[i])
-
-        if (motUtilisateur === listeMots[i]) {
-            console.log("Bravo !")
-            score++
-        } else {
-            console.log('Vous avez fait une erreur de frappe.')
-        }
+function choisirPhrasesOuMots(){
+    let choixUtilisateur = prompt('Choisissez le mode de jeu en tapant "mots" ou "phrases" : ')
+    while(choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
+        choixUtilisateur = prompt('Le choix n\'est pas valide. Choisissez le mode de jeu en tapant "mots" ou "phrases" : ')
     }
-} else {
-    for (let i = 0; i < listePhrases.length; i++) {
-        let phraseUtilisateur = prompt('Entrez la phrase : ' + listePhrases[i])
-
-        if (phraseUtilisateur === listePhrases[i]) {
-            console.log("Bravo !")
-            score++
-        } else {
-            console.log("Vous avez fait une erreur de frappe.")
-        }
-    }
+    return choixUtilisateur
 }
-console.log("Votre score final est de : " +score)
 
+function lancerBoucleDeJeu(tableauMotsPhrases){
+    for (let i = 0; i < tableauMotsPhrases.length; i++) {
+        motUtilisateur = prompt('Entrez le mot : ' + tableauMotsPhrases[i])
+        if (motUtilisateur === tableauMotsPhrases[i]) {
+            score++
+        }
+    }
+    return score
+}
+
+function lancerJeu(){
+    let choix = choisirPhrasesOuMots()
+    let score = 0
+    let nbMotsProposes = 0
+
+    if (choix === 'mots') {
+        lancerBoucleDeJeu(listeMots)
+        nbMotsProposes = listeMots.length
+    } else {
+        lancerBoucleDeJeu(listePhrases)
+        nbMotsProposes = listePhrases.length
+    }
+    afficherResultat(score, nbMotsProposes)
+}
+
+lancerBoucleDeJeu()
