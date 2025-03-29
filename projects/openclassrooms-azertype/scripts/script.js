645198bf6b7d29d181fbe224c582ce1c4d2832b6
Rémy Viniacourt <remy.viniacourt@outlook.fr>
2025-03-29 07:58:09 +0100

    Fonctions terminées + séparation du code en plusieurs fichiers

--- projects/openclassrooms-azertype/scripts/script.js
+++ projects/openclassrooms-azertype/scripts/script.js
@@ -1,16 +1,20 @@
 function afficherResultat(resultat, nombreTotalMots) {
+    // On affiche le score de l'utilisateur
     console.log('Votre score est de ' + resultat + ' sur ' + nombreTotalMots)
 }
 
 function choisirPhrasesOuMots(){
+    // Tant que l'utilisateur n'a pas chosi "mots" ou "phrases", on lui redemande
     let choixUtilisateur = prompt('Choisissez le mode de jeu en tapant "mots" ou "phrases" : ')
     while(choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
-        choixUtilisateur = prompt('Le choix n\'est pas valide. Choisissez le mode de jeu en tapant "mots" ou "phrases" : ')
+        choixUtilisateur = prompt('Le choix n\'est pas valide. Veuillez choisir entre "mots" et "phrases" : ')
     }
     return choixUtilisateur
 }
 
 function lancerBoucleDeJeu(tableauMotsPhrases){
+    // On affiche tous les mots de la liste et on incrémente le score si l'utilisateur a tapé le bon mot / phrase
+    let score = 0
     for (let i = 0; i < tableauMotsPhrases.length; i++) {
         motUtilisateur = prompt('Entrez le mot : ' + tableauMotsPhrases[i])
         if (motUtilisateur === tableauMotsPhrases[i]) {
@@ -26,13 +30,11 @@ function lancerJeu(){
     let nbMotsProposes = 0
 
     if (choix === 'mots') {
-        lancerBoucleDeJeu(listeMots)
+        score = lancerBoucleDeJeu(listeMots)
         nbMotsProposes = listeMots.length
     } else {
-        lancerBoucleDeJeu(listePhrases)
+        score = lancerBoucleDeJeu(listePhrases)
         nbMotsProposes = listePhrases.length
     }
     afficherResultat(score, nbMotsProposes)
 }
-
-lancerBoucleDeJeu()
