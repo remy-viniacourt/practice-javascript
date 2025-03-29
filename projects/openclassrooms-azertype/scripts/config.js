29a91f83a3e3550b99a00337e7f64c40b48e029d
Rémy Viniacourt <remy.viniacourt@outlook.fr>
2025-03-29 08:19:07 +0100

    Récupération du code openclassroom pour partir du même code + ajout du css pour le visuel du site

--- projects/openclassrooms-azertype/scripts/config.js
+++ projects/openclassrooms-azertype/scripts/config.js
@@ -1,3 +1,10 @@
-// Liste des mots utilisés pour le jeu
-const listeMots = ['Bonjour', 'Salut', 'Coucou']
-const listePhrases = ['Bonjour, comment allez-vous ?', 'Salut, ça va ?', 'Coucou, ça va ?']
+/*********************************************************************************
+ *
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ *
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
--- projects/openclassrooms-azertype/scripts/script.js
+++ projects/openclassrooms-azertype/scripts/script.js
@@ -1,40 +1,69 @@
-function afficherResultat(resultat, nombreTotalMots) {
-    // On affiche le score de l'utilisateur
-    console.log('Votre score est de ' + resultat + ' sur ' + nombreTotalMots)
+/*********************************************************************************
+ *
+ * Ce fichier contient toutes les fonctions nécessaires au fonctionnement du jeu.
+ *
+ *********************************************************************************/
+
+/**
+ * Cette fonction affiche dans la console le score de l'utilisateur
+ * @param {number} score : le score de l'utilisateur
+ * @param {number} nbMotsProposes : le nombre de mots proposés à l'utilisateur
+ */
+function afficherResultat(score, nbMotsProposes) {
+    console.log("Votre score est de " + score + " sur " + nbMotsProposes)
 }
 
-function choisirPhrasesOuMots(){
-    // Tant que l'utilisateur n'a pas chosi "mots" ou "phrases", on lui redemande
-    let choixUtilisateur = prompt('Choisissez le mode de jeu en tapant "mots" ou "phrases" : ')
-    while(choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
-        choixUtilisateur = prompt('Le choix n\'est pas valide. Veuillez choisir entre "mots" et "phrases" : ')
+/**
+ * Cette fonction demande à l'utilisateur de choisir entre "mots" et "phrases" et retourne le choix de l'utilisateur
+ * @return {string} : le choix de l'utilisateur, ce choix est nécessairement "mots" ou "phrases
+ */
+function choisirPhrasesOuMots() {
+    // Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix
+    let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+    while (choix !== "mots" && choix !== "phrases") {
+        choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
     }
-    return choixUtilisateur
+    return choix
 }
 
-function lancerBoucleDeJeu(tableauMotsPhrases){
-    // On affiche tous les mots de la liste et on incrémente le score si l'utilisateur a tapé le bon mot / phrase
+/**
+ * Cette fonction lance la boucle de jeu, c'est à dire qu'elle demande à l'utilisateur de saisir tous les mots
+ * contenus dans le tableau listePropositions. A chaque mot saisi, on incrémente le score de l'utilisateur
+ *
+ * @param {array[string]} listePropositions
+ * @return {number} : le score de l'utilisateur
+ */
+function lancerBoucleDeJeu(listePropositions) {
     let score = 0
-    for (let i = 0; i < tableauMotsPhrases.length; i++) {
-        motUtilisateur = prompt('Entrez le mot : ' + tableauMotsPhrases[i])
-        if (motUtilisateur === tableauMotsPhrases[i]) {
+    for (let i = 0; i < listePropositions.length; i++) {
+        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
+        let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
+        if (motUtilisateur === listePropositions[i]) {
+            // Si le mot saisi par l'utilisateur est correct, on incrémente le score
             score++
         }
     }
     return score
 }
 
-function lancerJeu(){
+/**
+ * Cette fonction lance le jeu.
+ * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
+ */
+function lancerJeu() {
+    // Initialisations
     let choix = choisirPhrasesOuMots()
     let score = 0
     let nbMotsProposes = 0
 
-    if (choix === 'mots') {
+    // On détermine la liste des mots ou des phrases à proposer à l'utilisateur
+    if (choix === "mots") {
         score = lancerBoucleDeJeu(listeMots)
         nbMotsProposes = listeMots.length
     } else {
         score = lancerBoucleDeJeu(listePhrases)
         nbMotsProposes = listePhrases.length
     }
+
     afficherResultat(score, nbMotsProposes)
 }
