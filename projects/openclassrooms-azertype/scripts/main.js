68ab9e641963aee4fa5c36827a01fcc170163368
Rémy Viniacourt <remy.viniacourt@outlook.fr>
2025-03-30 09:33:56 +0200

    Exercices sur la sélection de différents éléments + Affichage du score dans la page HTML et non dans la console

--- projects/openclassrooms-azertype/scripts/main.js
+++ projects/openclassrooms-azertype/scripts/main.js
@@ -4,4 +4,23 @@
  *
  *********************************************************************************/
 
-// lancerJeu()
+lancerJeu()
+
+// Exercices sur la sélection d'éléments
+
+/*
+let inputEcriture = document.getElementById("inputEcriture");
+console.log(inputEcriture);
+
+let btnValiderMot = document.getElementById("btnValiderMot");
+console.log(btnValiderMot);
+
+let divZoneProposition = document.querySelector(".zoneProposition");
+console.log(divZoneProposition);
+
+let spanScore = document.querySelector(".zoneScore span");
+console.log(spanScore);
+
+let listeBtnRadio  = document.querySelectorAll(".optionSource input");
+console.log(listeBtnRadio);
+ */
--- projects/openclassrooms-azertype/scripts/script.js
+++ projects/openclassrooms-azertype/scripts/script.js
@@ -5,12 +5,14 @@
  *********************************************************************************/
 
 /**
- * Cette fonction affiche dans la console le score de l'utilisateur
+ * Cette fonction affiche le score de l'utilisateur
  * @param {number} score : le score de l'utilisateur
  * @param {number} nbMotsProposes : le nombre de mots proposés à l'utilisateur
  */
 function afficherResultat(score, nbMotsProposes) {
-    console.log("Votre score est de " + score + " sur " + nbMotsProposes)
+    let spanScore = document.querySelector(".zoneScore span");
+    let afficherScore = `${score} / ${nbMotsProposes}`;
+    spanScore.innerText = afficherScore;
 }
 
 /**
